b5613c522e4db39e6ab73a0e034bb94695c4bad8
lydurh <lydur100@gmail.com>
2023-04-26 13:41:29 +0200

    implamentet slider in app.js and installed slide plugin

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(50);
+
+  const handleSliderChange = (newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className="App">
+      <h1>Slider Value: {value}</h1>
+      <Slider
+        min={0}
+        max={100}
+        step={1}
+        value={value}
+        onChange={handleSliderChange}
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
